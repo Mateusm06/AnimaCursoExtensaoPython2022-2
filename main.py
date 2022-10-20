d5b1d94441484fd73878aeb9989b95d0f0a37cd9
Mateus Maia <116230340+Mateusm06@users.noreply.github.com>
2022-10-20 01:20:09 +0000

    Aprendendo a criar e exibir variáveis na tela.

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-print('Hello World!')
+print('\033[34mHello World!')
+nome = str(input('Qual o seu nome? ')).title()
+idade = int(input('Qual a sua idade? '))
+print(f'Seu nome é {nome} e você tem {idade} anos')
