291ac02cea8ddb03c182309ca3b529d778702c9a
Mateus Maia <116230340+Mateusm06@users.noreply.github.com>
2022-11-17 00:02:46 +0000

    Utilizei a biblioteca sqlite3 para me conectar a um banco de dados.

---
+++ aula4-2022-11-16a.py
@@ -0,0 +1,12 @@
+# Importando a biblioteca sqlite3
+import sqlite3
+# Estabelecendo uma conexão com o banco de dados
+conexao = sqlite3.connect('dc_universe.db') 
+# Criar um objeto do tipo cursor
+cursor = conexao.cursor()
+# Comando sql do banco
+sql = 'SELECT pessoa_id, nome, nome_civil, tipo FROM pessoas'
+cursor.execute(sql)
+pessoas = cursor.fetchall()
+for pessoa in pessoas:
+  print(pessoa[1])
